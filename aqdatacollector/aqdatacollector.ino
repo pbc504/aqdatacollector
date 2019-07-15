f0000b1e7dd7b572df818eec7c99733eb23b9dc1
pbc504 <pbc504@york.ac.uk>
2019-07-15 12:18:44 +0100

    added coments and tidied up the code

--- aqdatacollector/aqdatacollector.ino
+++ aqdatacollector/aqdatacollector.ino
@@ -1,3 +1,5 @@
+// This program takes measurements every 2 seconds for NO, CO, Temperature and Relative Humidity and stores it on a SD card with a timestamp.
+
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_ADS1015.h>
@@ -5,35 +7,23 @@
 #include <SD.h>
 
 
-const int chipSelect = 10;
+const int chipSelect = 10;          // SD location
+Adafruit_ADS1115 ads1115NO(0x48);   // NO sensor memory location
+Adafruit_ADS1115 ads1115CO(0x49);   // CO sensor memory location
+int temperature_input = A0;         // Temperature input pin
+int humidity_input = A1;            // Relative Humidity input pin
+RTC_DS3231 rtc;                     // Define real time clock
 
 
-Adafruit_ADS1115 ads1115NO(0x48);
-Adafruit_ADS1115 ads1115CO(0x49);
-
-int temperature_input = A0; // temperature
-int humidity_input = A1; // humidity
-
-RTC_DS3231 rtc;
-
-
+// Setup serial and SD interface. Initializes the ADCs.
 void setup() {
-  // put your setup code here, to run once:
 
-  
   #ifndef ESP8266
-
   while (!Serial); // for Leonardo/Micro/Zero
-
   #endif
 
- 
-
-
-
   Serial.begin(9600);
-
-   Serial.print("Initializing SD card...");
+  Serial.print("Initializing SD card...");
 
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
@@ -43,16 +33,16 @@ void setup() {
   }
   Serial.println("card initialized.");
 
-
-  
   Serial.println("Hello!");
-  delay(3000);
+  delay(2000);
   ads1115NO.begin();
   ads1115CO.begin();
 }
 
+
+// Takes reading repeatedly every 2 seconds:
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   static const unsigned long REFRESH_INTERVAL = 2000; // ms
   static unsigned long lastRefreshTime = 0;
 
@@ -64,50 +54,52 @@ void loop() {
 
 }
 
+
+// Records all the data points, stores them in a string. Then writes the string to a text file in the SD card
 void takeReadings(){
 
-  String readings = "";
+  DateTime now = rtc.now(); //gets current time
 
-  DateTime now = rtc.now();
-  
+  // Definning sensors output values
   int16_t differential_01NO, differential_23NO, differential_01CO, differential_23CO;
-  differential_01NO = ads1115NO.readADC_Differential_0_1();
+  // Reads the differential between the different 4 electrodes for both CO and NO sensors
+  differential_01NO = ads1115NO.readADC_Differential_0_1();                            
   differential_23NO = ads1115NO.readADC_Differential_2_3();
   differential_01CO = ads1115CO.readADC_Differential_0_1();
   differential_23CO = ads1115CO.readADC_Differential_2_3();
 
   
-  uint16_t temperatureValue = analogRead(temperature_input);
+  // Definning sensors output values
+  uint16_t temperatureValue = analogRead(temperature_input);   // Takes in an analog voltage reading
   uint16_t humidityValue = analogRead(humidity_input);
 
-  float tempValue = temperatureValue;
+  float tempValue = temperatureValue; // Coverts from uint16_t to float (decimal points)
   float humidValue = humidityValue;
 
-  
-  // 6144mV / 32767 = 0.1875, 2^15 = 23767 because the ADC is a 16 bit signed, first bit decides if negative or positive  
- Serial.print(now.unixtime()+ 3353);
- Serial.print(",");
- Serial.print(differential_01NO * 0.1875);
- Serial.print(",");
- Serial.print(differential_23NO * 0.1875);
- Serial.print(",");
- Serial.print(differential_01CO * 0.1875);
- Serial.print(",");
- Serial.print(differential_23CO * 0.1875);
- Serial.print(",");
- Serial.print(tempValue);
- Serial.print(",");
- Serial.println(humidValue);
-
-
- //-----------------------------------------------
-
-  readings.concat(now.unixtime()+ 3353);
+  // Creates the text file to log the data.
+  File dataFile = SD.open("reading1.txt", FILE_WRITE);
+  // Creates the string, readings, to be printed on the file reading1.txt on the SD card.
+  String readings = "";
   
 
- File dataFile = SD.open("reading1.txt", FILE_WRITE);
+// 6144mV / 32767 = 0.1875, 2^15 = 23767 because the ADC is a 16 bit signed, first bit decides if negative or positive
+// Appends each datapoint to the string
+ readings.concat(now.unixtime()+ 3353);
+ readings.concat(",");
+ readings.concat(differential_01NO * 0.1875);
+ readings.concat(",");
+ readings.concat(differential_23NO * 0.1875);
+ readings.concat(",");
+ readings.concat(differential_01CO * 0.1875);
+ readings.concat(",");
+ readings.concat(differential_23CO * 0.1875);
+ readings.concat(",");
+ readings.concat(tempValue);
+ readings.concat(",");
+ readings.concat(humidValue);
+  
 
- 
+// If the file has been creating, it writes the sting to the text file and prints to the serial aswell
  if (dataFile) {
     dataFile.println(readings);
     dataFile.close();

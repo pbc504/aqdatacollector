852140b2b90f71a865617eea46fd9de832c0f0d0
pbc504 <pbc504@york.ac.uk>
2019-07-15 10:44:12 +0100

    added unixtime

--- aqdatacollector/aqdatacollector.ino
+++ aqdatacollector/aqdatacollector.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include <SPI.h>
 #include <Adafruit_ADS1015.h>
 #include "RTClib.h"
 
@@ -6,6 +7,8 @@
 Adafruit_ADS1115 ads1115NO(0x48);
 Adafruit_ADS1115 ads1115CO(0x49);
 
+int temperature_input = A0; // temperature
+int humidity_input = A1; // humidity
 
 RTC_DS3231 rtc;
 
@@ -29,22 +32,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  
-
+DateTime now = rtc.now();
   
   int16_t differential_01NO, differential_23NO, differential_01CO, differential_23CO;
-  
   differential_01NO = ads1115NO.readADC_Differential_0_1();
   differential_23NO = ads1115NO.readADC_Differential_2_3();
   differential_01CO = ads1115CO.readADC_Differential_0_1();
   differential_23CO = ads1115CO.readADC_Differential_2_3();
 
+  uint16_t temperatureValue = analogRead(temperature_input);
+  uint16_t humidityValue = analogRead(humidity_input);
 
+  float tempValue = temperatureValue;
+  float humidValue = humidityValue;
 
   
   // 6144mV / 32767 = 0.1875, 2^15 = 23767 because the ADC is a 16 bit signed, first bit decides if negative or positive  
- Serial.print("timestamp");
+ Serial.print(now.unixtime()+ 3353);
  Serial.print(",");
  Serial.print(differential_01NO * 0.1875);
  Serial.print(",");
@@ -54,40 +58,9 @@ void loop() {
  Serial.print(",");
  Serial.print(differential_23CO * 0.1875);
  Serial.print(",");
- Serial.print("temperature");
+ Serial.print(tempValue);
  Serial.print(",");
- Serial.println("humidity");
+ Serial.println(humidValue);
  
-  delay(2000);
-}
-
-DateTime getTime(){
-  DateTime now = rtc.now() + TimeSpan(0,0,56,6);
-
-  
-   Serial.print(now.year(), DEC);
-
-    Serial.print('/');
-
-    Serial.print(now.month(), DEC);
-
-    Serial.print('/');
-
-    Serial.print(now.day(), DEC);
-
-    Serial.print(' ');
-
-    Serial.print(now.hour(), DEC);
-
-    Serial.print(':');
-
-    Serial.print(now.minute(), DEC);
-
-    Serial.print(':');
-
-    Serial.print(now.second(), DEC);
-
-    Serial.println();
-    Serial.println();
 
 }
